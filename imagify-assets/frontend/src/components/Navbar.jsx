e1632635222adf967f9ec0b64971a845272468fc
PRASHANT1603 <136425600+PRASHANT1603@users.noreply.github.com>
2025-12-20 00:00:39 +0530

    Image generater by text

---
+++ imagify-assets/frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  const [User, setUser] = useState(null);
+
+
+  return (
+    <div className='flex items-center justify-between py-4' >
+      <Link to='/'><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40 '/></Link>
+
+    <div>
+      {User ?
+      <div></div> 
+      :
+      <div className='flex items-center gap-2 sm:gap-5'> 
+        <p className='cursor-pointer'>Pricing</p>
+        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-        rounded-full'>Login</button>
+      </div>
+      }
+      
+     
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ imagify-assets/frontend/src/pages/Creadit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Creadit = () => {
+  return (
+    <div>
+      But Creadit
+    </div>
+  )
+}
+
+export default Creadit
