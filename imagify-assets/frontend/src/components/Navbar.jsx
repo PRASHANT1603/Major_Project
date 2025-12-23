a96a4312c7df395def781eb17e623b9a31c124ea
PRASHANT1603 <136425600+PRASHANT1603@users.noreply.github.com>
2025-12-23 15:00:43 +0530

    frontend complete

--- imagify-assets/frontend/src/components/Navbar.jsx
+++ imagify-assets/frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   // const [User, setUser] = useState(true);
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
 
   return (
@@ -46,7 +46,7 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-        rounded-full">
+            <button onClick={()=>setShowLogin(true)} className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
               Login
             </button>
           </div>
